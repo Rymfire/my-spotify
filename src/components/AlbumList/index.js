3eaf8cd060ea3ee944c22b8e577b803b92d5ecfc
Loic POTHIN <loic.pothin@epitech.eu>
2020-11-02 16:00:14 +0400

    Grid for artist page

--- src/components/AlbumList/index.js
+++ src/components/AlbumList/index.js
@@ -1,15 +1,37 @@
 import React, {Component} from "react";
 import SingleAlbum from "./SingleAlbum";
+import {Grid} from "@material-ui/core";
 
 export default class AlbumList extends Component {
     render() {
         if (this.props.value && Object.keys(this.props.value).length !== 0) {
             const albumFormatedList = (() => {
-                return this.props.value.items.map(value => (value.album_type === 'album') ? <SingleAlbum history={this.props.history} value={value}/> : null).filter(value => value !== null);
+                return this.props.value.items.map(value => {
+                    return (value.album_type === 'album')
+                        ? <Grid
+                            item
+                            xs={6}
+                            md={3}
+                            xl={2}
+                        >
+                            <SingleAlbum history={this.props.history} value={value}/>
+                        </Grid>
+                        : null
+                }).filter(value => value !== null);
             })();
             return (
                 <div>
+                    <Grid
+                        container
+                        spacing={2}
+                        style={{
+                            width: '100%',
+                            margin: '0px',
+                        }}
+                        alignItems="stretch"
+                    >
                     {albumFormatedList}
+                    </Grid>
                 </div>
             );
         } else {
